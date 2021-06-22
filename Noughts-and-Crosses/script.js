a2be576127de707c4c734266883f4725917be235
ronaldc5184 <85898357+ronaldc5184@users.noreply.github.com>
2021-06-22 07:59:37 -0700

    Add files via upload

--- Noughts-and-Crosses/script.js
+++ Noughts-and-Crosses/script.js
@@ -13,6 +13,8 @@ for (i = 0; i < 9; i++) {
 	b[i].addEventListener("click", takeTurn);
 }
 b[9].addEventListener("click", function(){location.reload();});
+b[10].addEventListener("click", function(){document.querySelector('#container').style.display = "none"});
+b[11].addEventListener("click", function(){document.querySelector('#container2').style.display = "none"});
 
 function takeTurn() {
 	if (currentPlayer == 1 && this.innerHTML == "") {
@@ -44,13 +46,21 @@ function checkWin() {
 	}
 	if (turns == 9 && ended == 0) {
 		document.body.style.backgroundImage = 'url("cat.gif")';
+		document.querySelector('#container').style.display = "flex";
 	}
 	
 }
 
 function announceWin(winner) {
-	alert(winner + " wins!");
+	document.querySelector('#container2').style.display = "flex";
+	document.querySelector('#wintext').innerHTML = winner + " wins!"
+	if (winner == "X") {
+		document.querySelector('#wintext').style.color = "red";
+	} else {
+		document.querySelector('#wintext').style.color = "lime";
+	}
 	for (i = 0; i < 9; i++) {
 		b[i].removeEventListener("click", takeTurn);
 	}
 }
+
